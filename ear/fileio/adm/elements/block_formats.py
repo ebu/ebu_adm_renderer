b5fd9287f6d3c5ee5087f8f5dc8b053b7f5db92d
Tom Nixon <tom@tomn.co.uk>
2021-07-29 19:30:58 +0100

    docs: document block_formats

--- ear/fileio/adm/elements/block_formats.py
+++ ear/fileio/adm/elements/block_formats.py
@@ -8,6 +8,14 @@ from .main_elements import AudioChannelFormat, TypeDefinition
 
 @attrs(slots=True)
 class AudioBlockFormat(object):
+    """ADM audioBlockFormat base class
+
+    Attributes:
+        id (Optional[str])
+        rtime (Optional[fractions.Fraction])
+        duration (Optional[fractions.Fraction])
+    """
+
     id = attrib(default=None)
     rtime = attrib(validator=optional(instance_of(Fraction)), default=None)
     duration = attrib(validator=optional(instance_of(Fraction)), default=None)
@@ -25,6 +33,18 @@ BlockFormat = AudioBlockFormat
 
 @attrs(slots=True)
 class MatrixCoefficient(object):
+    """ADM audioBlockFormat Matrix coefficient element
+
+    Attributes:
+        inputChannelFormat (Optional[AudioChannelFormat])
+        gain (Optional[float])
+        gainVar (Optional[str])
+        phase (Optional[float])
+        phaseVar (Optional[str])
+        delay (Optional[float])
+        delayVar (Optional[str])
+    """
+
     inputChannelFormat = attrib(default=None, validator=optional(instance_of(AudioChannelFormat)))
 
     gain = attrib(default=None, validator=optional(instance_of(float)))
@@ -49,6 +69,13 @@ class MatrixCoefficient(object):
 
 @attrs(slots=True)
 class AudioBlockFormatMatrix(AudioBlockFormat):
+    """ADM audioBlockFormat with typeDefinition == "Matrix"
+
+    Attributes:
+        outputChannelFormat (Optional[AudioChannelFormat])
+        matrix (list[MatrixCoefficient])
+    """
+
     outputChannelFormat = attrib(default=None, validator=optional(instance_of(AudioChannelFormat)))
     matrix = attrib(default=Factory(list), validator=list_of(MatrixCoefficient))
 
@@ -70,11 +97,25 @@ class AudioBlockFormatMatrix(AudioBlockFormat):
 
 @attrs(slots=True)
 class ChannelLock(object):
+    """ADM channelLock element
+
+    Attributes:
+        maxDistance (Optional[float])
+    """
+
     maxDistance = attrib(default=None, validator=optional(instance_of(float)))
 
 
 @attrs(slots=True)
 class ObjectDivergence(object):
+    """ADM objectDivergence element
+
+    Attributes:
+        value (float)
+        azimuthRange (Optional[float])
+        positionRange (Optional[float])
+    """
+
     value = attrib(validator=instance_of(float))
     azimuthRange = attrib(default=None, validator=optional(instance_of(float)))
     positionRange = attrib(default=None, validator=optional(instance_of(float)))
@@ -82,12 +123,30 @@ class ObjectDivergence(object):
 
 @attrs(slots=True)
 class JumpPosition(object):
+    """ADM jumpPosition element
+
+    Attributes:
+        flag (bool): contents of the jumpPosition element
+        interpolationLength (Optional[fractions.Fraction])
+    """
+
     flag = attrib(default=False, validator=instance_of(bool))
     interpolationLength = attrib(default=None, validator=optional(instance_of(Fraction)))
 
 
 @attrs(slots=True)
 class CartesianZone(object):
+    """ADM zoneExclusion zone element with Cartesian coordinates
+
+    Attributes:
+        minX (float)
+        minY (float)
+        minZ (float)
+        maxX (float)
+        maxY (float)
+        maxZ (float)
+    """
+
     minX = attrib(validator=instance_of(float))
     minY = attrib(validator=instance_of(float))
     minZ = attrib(validator=instance_of(float))
@@ -98,6 +157,15 @@ class CartesianZone(object):
 
 @attrs(slots=True)
 class PolarZone(object):
+    """ADM zoneExclusion zone element with polar coordinates
+
+    Attributes:
+        minElevation (float)
+        maxElevation (float)
+        minAzimuth (float)
+        maxAzimuth (float)
+    """
+
     minElevation = attrib(validator=instance_of(float))
     maxElevation = attrib(validator=instance_of(float))
     minAzimuth = attrib(validator=instance_of(float))
@@ -106,6 +174,24 @@ class PolarZone(object):
 
 @attrs(slots=True)
 class AudioBlockFormatObjects(AudioBlockFormat):
+    """ADM audioBlockFormat with typeDefinition == "Objects"
+
+    Attributes:
+        position (Optional[ObjectPosition])
+        cartesian (bool)
+        width (float)
+        height (float)
+        depth (float)
+        gain (float)
+        diffuse (float)
+        channelLock (Optional[ChannelLock])
+        objectDivergence (Optional[ObjectDivergence])
+        jumpPosition (JumpPosition)
+        screenRef (bool)
+        importance (int)
+        zoneExclusion (list[Union[CartesianZone, PolarZone]])
+    """
+
     position = attrib(default=None, validator=instance_of(ObjectPosition), converter=convert_object_position)
     cartesian = attrib(converter=bool, default=False)
     width = attrib(converter=float, default=0.)
@@ -123,12 +209,30 @@ class AudioBlockFormatObjects(AudioBlockFormat):
 
 @attrs(slots=True)
 class AudioBlockFormatDirectSpeakers(AudioBlockFormat):
+    """ADM audioBlockFormat with typeDefinition == "DirectSpeakers"
+
+    Attributes:
+        position (DirectSpeakerPosition)
+        speakerLabel (list[str])
+    """
+
     position = attrib(default=None, validator=instance_of(DirectSpeakerPosition))
     speakerLabel = attrib(default=Factory(list))
 
 
 @attrs(slots=True)
 class AudioBlockFormatHoa(AudioBlockFormat):
+    """ADM audioBlockFormat with typeDefinition == "HOA"
+
+    Attributes:
+        equation (Optional[str])
+        order (Optional[int])
+        degree (Optional[int])
+        normalization (Optional[str])
+        nfcRefDist (Optional[float])
+        screenRef (Optional[bool])
+    """
+
     equation = attrib(default=None, validator=optional(instance_of(str)))
     order = attrib(default=None, validator=optional(instance_of(int)))
     degree = attrib(default=None, validator=optional(instance_of(int)))
