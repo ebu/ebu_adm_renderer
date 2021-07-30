23f090775376550ab13a6a3dd1643e4d044b20c3
Tom Nixon <tom@tomn.co.uk>
2021-07-30 15:23:06 +0100

    docs: document load_common_definitions

--- ear/fileio/adm/common_definitions.py
+++ ear/fileio/adm/common_definitions.py
@@ -4,6 +4,12 @@ import lxml.etree
 
 
 def load_common_definitions(adm):
+    """Load the common definitions file from IRU-R Rec. BS.2094-1, setting
+    is_common_definition=True on all loaded elements.
+
+    Parameters:
+        adm (ADM): ADM structure to add to.
+    """
     fname = "data/2094_common_definitions.xml"
     with pkg_resources.resource_stream(__name__, fname) as stream:
         element = lxml.etree.parse(stream)
