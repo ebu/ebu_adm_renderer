7baa24c70e29e6fe9ae9611d33adc9e53d631ac8
Tom Nixon <tom@tomn.co.uk>
2021-09-28 11:47:16 +0100

    run black on ambix_to_bwf

--- ear/cmdline/ambix_to_bwf.py
+++ ear/cmdline/ambix_to_bwf.py
@@ -2,8 +2,19 @@ import numpy as np
 import lxml.etree
 from ..core.hoa import from_acn
 from ..fileio.adm.adm import ADM
-from ..fileio.adm.elements import AudioBlockFormatHoa, AudioChannelFormat, TypeDefinition, FormatDefinition
-from ..fileio.adm.elements import AudioStreamFormat, AudioTrackFormat, AudioPackFormat, AudioObject, AudioTrackUID
+from ..fileio.adm.elements import (
+    AudioBlockFormatHoa,
+    AudioChannelFormat,
+    TypeDefinition,
+    FormatDefinition,
+)
+from ..fileio.adm.elements import (
+    AudioStreamFormat,
+    AudioTrackFormat,
+    AudioPackFormat,
+    AudioObject,
+    AudioTrackUID,
+)
 from ..fileio.adm.chna import populate_chna_chunk
 from ..fileio.adm.generate_ids import generate_ids
 from ..fileio.adm.xml import adm_to_xml
@@ -12,11 +23,20 @@ from ..fileio.bw64.chunks import ChnaChunk, FormatInfoChunk
 
 
 def add_args(subparsers):
-    subparser = subparsers.add_parser("ambix_to_bwf", help="make a BWF file from an ambix format HOA file")
+    subparser = subparsers.add_parser(
+        "ambix_to_bwf", help="make a BWF file from an ambix format HOA file"
+    )
     subparser.add_argument("--norm", default="SN3D", help="normalization mode")
-    subparser.add_argument("--nfcDist", type=float, default=None, help="Near-Field Compensation Distance (float)")
+    subparser.add_argument(
+        "--nfcDist",
+        type=float,
+        default=None,
+        help="Near-Field Compensation Distance (float)",
+    )
     subparser.add_argument("--screenRef", help="Screen Reference", action="store_true")
-    subparser.add_argument("--chna-only", help="use only CHNA with common definitions", action="store_true")
+    subparser.add_argument(
+        "--chna-only", help="use only CHNA with common definitions", action="store_true"
+    )
     subparser.add_argument("input", help="input file")
     subparser.add_argument("output", help="output BWF file")
     subparser.set_defaults(command=ambix_to_bwf)
@@ -91,23 +111,30 @@ def build_adm(acn, norm, nfcDist, screenRef):
 
 def build_adm_common_defs(acns, norm):
     from ..fileio.adm.common_definitions import load_common_definitions
+
     adm = ADM()
     load_common_definitions(adm)
 
     order, degree = from_acn(acns)
 
     pack_name = "3D_order{order}_{norm}_ACN".format(order=max(order), norm=norm)
-    [pack_format] = [apf for apf in adm.audioPackFormats if apf.audioPackFormatName == pack_name]
+    [pack_format] = [
+        apf for apf in adm.audioPackFormats if apf.audioPackFormatName == pack_name
+    ]
 
     for channel_no, acn in enumerate(acns, 1):
         track_name = "PCM_{norm}_ACN_{acn}".format(norm=norm, acn=acn)
-        [track_format] = [tf for tf in adm.audioTrackFormats if tf.audioTrackFormatName == track_name]
+        [track_format] = [
+            tf for tf in adm.audioTrackFormats if tf.audioTrackFormatName == track_name
+        ]
 
-        adm.addAudioTrackUID(AudioTrackUID(
-            trackIndex=channel_no,
-            audioTrackFormat=track_format,
-            audioPackFormat=pack_format,
-        ))
+        adm.addAudioTrackUID(
+            AudioTrackUID(
+                trackIndex=channel_no,
+                audioTrackFormat=track_format,
+                audioPackFormat=pack_format,
+            )
+        )
 
     return adm
 
@@ -134,13 +161,18 @@ def ambix_to_bwf(args):
         chna = ChnaChunk()
         populate_chna_chunk(chna, adm)
 
-        fmtInfo = FormatInfoChunk(formatTag=1,
-                                  channelCount=infile.channels,
-                                  sampleRate=infile.sampleRate,
-                                  bitsPerSample=infile.bitdepth)
+        fmtInfo = FormatInfoChunk(
+            formatTag=1,
+            channelCount=infile.channels,
+            sampleRate=infile.sampleRate,
+            bitsPerSample=infile.bitdepth,
+        )
 
-        with openBw64(args.output, 'w', chna=chna, formatInfo=fmtInfo, axml=axml) as outfile:
+        with openBw64(
+            args.output, "w", chna=chna, formatInfo=fmtInfo, axml=axml
+        ) as outfile:
             while True:
                 samples = infile.read(1024)
-                if samples.shape[0] == 0: break
+                if samples.shape[0] == 0:
+                    break
                 outfile.write(samples)
