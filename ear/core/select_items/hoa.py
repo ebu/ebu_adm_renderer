4038e6f0562282f7e02f732c068cb9e67b539bc7
Tom Nixon <tom@tomn.co.uk>
2021-11-05 14:36:05 +0000

    reformat hoa

--- ear/core/select_items/hoa.py
+++ ear/core/select_items/hoa.py
@@ -5,6 +5,7 @@ from ...fileio.adm.exceptions import AdmError
 # functions which take a path through the audioPackFormats and an
 # audioChannelFormat, and extract some parameter for a single HOA channel
 
+
 def _get_pack_param(audioPackFormat_path, audioChannelFormat, name, default=None):
     """Get a parameter which can be defined in either audioPackFormats or
     audioBlockFormats. Any specified parameters must be consistent. If none
@@ -18,11 +19,13 @@ def _get_pack_param(audioPackFormat_path, audioChannelFormat, name, default=None
 
     if not_none:
         if any(value != not_none[0] for value in not_none):
-            raise AdmError("Conflicting {name} values in path from {apf.id} to {acf.id}".format(
-                name=name,
-                apf=audioPackFormat_path[0],
-                acf=audioChannelFormat,
-            ))
+            raise AdmError(
+                "Conflicting {name} values in path from {apf.id} to {acf.id}".format(
+                    name=name,
+                    apf=audioPackFormat_path[0],
+                    acf=audioChannelFormat,
+                )
+            )
 
         return not_none[0]
     else:
@@ -51,6 +54,7 @@ get_duration = partial(_get_block_format_attr, attr="duration")
 
 # functions to use the above definitions for multiple channels
 
+
 def get_single_param(pack_paths_channels, name, get_param):
     """Get one parameter which must be consistent in all channels.
 
@@ -62,16 +66,22 @@ def get_single_param(pack_paths_channels, name, get_param):
         get_param (callable): function from (audioPackFormat_path,
             audioChannelFormat) to the value of the parameter.
     """
-    for pack_path_channel_a, pack_path_channel_b in zip(pack_paths_channels[:-1], pack_paths_channels[1:]):
+    for pack_path_channel_a, pack_path_channel_b in zip(
+        pack_paths_channels[:-1], pack_paths_channels[1:]
+    ):
         pack_format_path_a, channel_a = pack_path_channel_a
         pack_format_path_b, channel_b = pack_path_channel_b
-        if get_param(pack_format_path_a, channel_a) != get_param(pack_format_path_b, channel_b):
-            raise AdmError("All HOA audioChannelFormats in a single audioPackFormat must "
-                           "share the same {name} value, but {acf_a.id} and {acf_b.id} differ.".format(
-                               name=name,
-                               acf_a=channel_a,
-                               acf_b=channel_b,
-                           ))
+        if get_param(pack_format_path_a, channel_a) != get_param(
+            pack_format_path_b, channel_b
+        ):
+            raise AdmError(
+                "All HOA audioChannelFormats in a single audioPackFormat must "
+                "share the same {name} value, but {acf_a.id} and {acf_b.id} differ.".format(
+                    name=name,
+                    acf_a=channel_a,
+                    acf_b=channel_b,
+                )
+            )
 
     pack_path, channel = pack_paths_channels[0]
     return get_param(pack_path, channel)
